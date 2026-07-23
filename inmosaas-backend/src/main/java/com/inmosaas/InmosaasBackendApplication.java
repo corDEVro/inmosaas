c8a34b64c232724251d136bff49d2b828610dff7
Juan Manuel Cordero <jm.corderoganfornina@gmail.com>
2026-07-23 19:28:21 +0200

    feat: inicializar backend spring boot y conexion exitosa a postgresql

---
+++ inmosaas-backend/src/main/java/com/inmosaas/InmosaasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inmosaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmosaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmosaasBackendApplication.class, args);
+	}
+
+}
---
+++ inmosaas-backend/src/test/java/com/inmosaas/InmosaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmosaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmosaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
